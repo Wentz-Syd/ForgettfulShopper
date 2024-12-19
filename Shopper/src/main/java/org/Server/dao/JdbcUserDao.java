837abd5604f8d0caa139a21e0c00712516ae3b05
Wentz-Syd <wentz.syd@gmail.com>
2024-12-19 01:54:07 -0500

    Registration working

--- Shopper/src/main/java/org/Server/dao/JdbcUserDao.java
+++ Shopper/src/main/java/org/Server/dao/JdbcUserDao.java
@@ -1,18 +1,77 @@
 package org.Server.dao;
 
+import org.Server.exception.DaoException;
 import org.Server.model.RegisterUserDto;
 import org.Server.model.User;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.jdbc.CannotGetJdbcConnectionException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class JdbcUserDao implements UserDao{
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public JdbcUserDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public List<User> getUsers() {
+        return List.of();
+    }
+
+    @Override
+    public User getUserById(int userId) {
+        User user = null;
+        String sql = "SELECT user_id, username, password_hash FROM users WHERE user_id = ?";
+        try{
+            SqlRowSet results = jdbcTemplate.queryForRowSet(sql, userId);
+            if (results.next()){
+                user = mapRowToUser(results);
+            }
+        } catch (CannotGetJdbcConnectionException e) {
+            throw new DaoException("Unable to connect to server or database", e);
+        }
+        return user;
+    }
+
     @Override
     public User createUser(RegisterUserDto user) {
-        return null;
+
+       User newUser = null;
+       String sql = "INSERT INTO users (username, password_hash) VALUES (?,?) RETURNING user_id";
+       String password_hash = new BCryptPasswordEncoder().encode(user.getPassword());
+       try{
+           int newUserId = jdbcTemplate.queryForObject(sql, int.class, user.getUsername(),password_hash);
+           newUser = getUserById(newUserId);
+       } catch (CannotGetJdbcConnectionException e) {
+           throw new DaoException("Unable to connect to server or database", e);
+       } catch (DataIntegrityViolationException e) {
+           throw new DaoException("Data integrity violation", e);
+       }
+        return newUser;
     }
 
     @Override
     public User getUserByUsername(String username) {
         return null;
     }
+
+
+    private User mapRowToUser(SqlRowSet rs){
+        User user = new User();
+        user.setId(rs.getInt("user_id"));
+        user.setUsername(rs.getString("username"));
+        user.setPassword(rs.getString("password_hash"));
+        user.setActivated(true);
+        return user;
+    }
+
+
 }
--- Shopper/src/main/java/org/Server/dao/UserDao.java
+++ Shopper/src/main/java/org/Server/dao/UserDao.java
@@ -4,8 +4,14 @@ import org.Server.model.RegisterUserDto;
 import org.Server.model.User;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 
 public interface UserDao {
+
+    List<User> getUsers();
+
+    User getUserById(int userId);
 
     User createUser(RegisterUserDto user);
 
